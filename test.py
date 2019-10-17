7eaf64fd4fa08e7492af7d147b9a5b0783ff41cf
Garrett Luskey <garrett.luskey@gmail.com>
2019-10-17 17:22:20 -0500

    Worked on getting inputs and outputs formatted for network training

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow_datasets as tfds
 import tensorflow as tf
+from transformer import *
 
 import time
 import numpy as np
@@ -11,9 +12,10 @@ import matplotlib.pyplot as plt
 import os, pickle
 from zipfile import ZipFile
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("./My Project 63888-29e738f88cfa.json")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("./My Project 63888-5efd88711631.json")
 
 from google.cloud import bigquery
+from optimizer import CustomSchedule
 
 client = bigquery.Client()
 
@@ -22,10 +24,11 @@ query = (
     "SELECT questions.id as `q_id`, questions.title as `q_title`, questions.body as `q_body`, answers.id as `a_id`, answers.title as `a_title`, answers.body as `a_body` FROM `bigquery-public-data.stackoverflow.posts_questions` AS `questions` "
     "INNER JOIN `bigquery-public-data.stackoverflow.posts_answers` AS `answers` "
     "ON questions.accepted_answer_id = answers.id "
-    "LIMIT 1000000"
+    "LIMIT 1000"
 )
 # Executes the query
 query_job = client.query(query)
+
 
 # for row in query_job.result(page_size=100):  # API request - fetches results
 #     # Row values can be accessed by field name or index
@@ -50,9 +53,16 @@ def printx(x, counter):
 
 # filehandler = open("./tokenizer_a.pkl", 'wb', pickle.HIGHEST_PROTOCOL)
 # pickle.dump(tokenizer_a, filehandler)
+LOAD_TOKENIZERS = True
+tokenizer_q = None
+tokenizer_a = None
 
-with ZipFile("./1m/tokenizer_q.zip").open("tokenizer_q.pkl", 'r') as filehandler:
-    tokenizer_q = pickle.load(filehandler)
+if LOAD_TOKENIZERS:
+    print("Loading question tokenizer...")
+    tokenizer_q = pickle.load(ZipFile("./1m/tokenizer_q.zip").open("tokenizer_q.pkl", 'r'))
+    print("Loading answer tokenizer...")
+    tokenizer_a = pickle.load(ZipFile("./1m/tokenizer_a.zip").open("tokenizer_a.pkl", 'r'))
+    print("Finished loading tokenizers")
 
     sample_string = '<p>Transformer is awesome.</p>'
 
@@ -67,3 +77,295 @@ with ZipFile("./1m/tokenizer_q.zip").open("tokenizer_q.pkl", 'r') as filehandler
     for ts in tokenized_string:
 
         print ('{} ----> {}'.format(ts, tokenizer_q.decode([ts])))
+
+else:
+    print("Set to not load tokenizers")
+
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+def encode(lang1, lang2):
+    lang1 = [tokenizer_q.vocab_size] + tokenizer_q.encode(
+        lang1.numpy()) + [tokenizer_q.vocab_size+1]
+
+    lang2 = [tokenizer_a.vocab_size] + tokenizer_a.encode(
+        lang2.numpy()) + [tokenizer_a.vocab_size+1]
+    
+    return lang1, lang2
+
+MAX_LENGTH = 40
+
+def filter_max_length(x, y, max_length=MAX_LENGTH):
+    return tf.logical_and(tf.size(x) <= max_length,
+                            tf.size(y) <= max_length)
+
+def tf_encode(q, a):
+    # NOTE may need to remove [] on q and a
+    return tf.py_function(encode, [[q], [a]], [tf.int64, tf.int64])
+
+
+pos_encoding = positional_encoding(50, 512)
+print (pos_encoding.shape)
+
+
+
+x = tf.constant([[7, 6, 0, 0, 1], [1, 2, 3, 0, 0], [0, 0, 0, 4, 5]])
+print(create_padding_mask(x))
+
+
+
+x = tf.random.uniform((1, 3))
+temp = create_look_ahead_mask(x.shape[1])
+print(temp)
+
+
+
+def print_out(q, k, v):
+    temp_out, temp_attn = scaled_dot_product_attention(
+        q, k, v, None)
+    print ('Attention weights are:')
+    print (temp_attn)
+    print ('Output is:')
+    print (temp_out)
+
+np.set_printoptions(suppress=True)
+
+temp_k = tf.constant([[10,0,0],
+                      [0,10,0],
+                      [0,0,10],
+                      [0,0,10]], dtype=tf.float32)  # (4, 3)
+
+temp_v = tf.constant([[   1,0],
+                      [  10,0],
+                      [ 100,5],
+                      [1000,6]], dtype=tf.float32)  # (4, 2)
+
+# This `query` aligns with the second `key`,
+# so the second `value` is returned.
+temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)  # (1, 3)
+print_out(temp_q, temp_k, temp_v)
+
+# This query aligns with a repeated key (third and fourth), 
+# so all associated values get averaged.
+temp_q = tf.constant([[0, 0, 10]], dtype=tf.float32)  # (1, 3)
+print_out(temp_q, temp_k, temp_v)
+
+# This query aligns equally with the first and second key, 
+# so their values get averaged.
+temp_q = tf.constant([[10, 10, 0]], dtype=tf.float32)  # (1, 3)
+print_out(temp_q, temp_k, temp_v)
+
+temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)  # (3, 3)
+print_out(temp_q, temp_k, temp_v)
+
+
+
+
+temp_mha = MultiHeadAttention(d_model=512, num_heads=8)
+y = tf.random.uniform((1, 60, 512))  # (batch_size, encoder_sequence, d_model)
+out, attn = temp_mha(y, k=y, q=y, mask=None)
+print(out.shape, attn.shape)
+
+
+
+sample_ffn = point_wise_feed_forward_network(512, 2048)
+print(sample_ffn(tf.random.uniform((64, 50, 512))).shape)
+
+sample_encoder_layer = EncoderLayer(512, 8, 2048)
+
+sample_encoder_layer_output = sample_encoder_layer(
+    tf.random.uniform((64, 43, 512)), False, None)
+
+print(sample_encoder_layer_output.shape)  # (batch_size, input_seq_len, d_model)
+
+sample_decoder_layer = DecoderLayer(512, 8, 2048)
+
+sample_decoder_layer_output, _, _ = sample_decoder_layer(
+    tf.random.uniform((64, 50, 512)), sample_encoder_layer_output, 
+    False, None, None)
+
+print(sample_decoder_layer_output.shape)  # (batch_size, target_seq_len, d_model)
+
+
+sample_encoder = Encoder(num_layers=2, d_model=512, num_heads=8, 
+                         dff=2048, input_vocab_size=8500,
+                         maximum_position_encoding=10000)
+temp_input = tf.random.uniform((64, 62), dtype=tf.int64, minval=0, maxval=200)
+
+sample_encoder_output = sample_encoder(temp_input, training=False, mask=None)
+
+print (sample_encoder_output.shape)  # (batch_size, input_seq_len, d_model)
+
+sample_decoder = Decoder(num_layers=2, d_model=512, num_heads=8, 
+                         dff=2048, target_vocab_size=8000,
+                         maximum_position_encoding=5000)
+temp_input = tf.random.uniform((64, 26), dtype=tf.int64, minval=0, maxval=200)
+
+output, attn = sample_decoder(temp_input, 
+                              enc_output=sample_encoder_output, 
+                              training=False,
+                              look_ahead_mask=None, 
+                              padding_mask=None)
+
+print(output.shape, attn['decoder_layer2_block2'].shape)
+
+sample_transformer = Transformer(
+    num_layers=2, d_model=512, num_heads=8, dff=2048, 
+    input_vocab_size=8500, target_vocab_size=8000, 
+    pe_input=10000, pe_target=6000)
+
+temp_input = tf.random.uniform((64, 38), dtype=tf.int64, minval=0, maxval=200)
+temp_target = tf.random.uniform((64, 36), dtype=tf.int64, minval=0, maxval=200)
+
+fn_out, _ = sample_transformer(temp_input, temp_target, training=False, 
+                               enc_padding_mask=None, 
+                               look_ahead_mask=None,
+                               dec_padding_mask=None)
+
+print(fn_out.shape)  # (batch_size, tar_seq_len, target_vocab_size)
+
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+
+input_vocab_size = tokenizer_a.vocab_size + 2
+target_vocab_size = tokenizer_q.vocab_size + 2
+dropout_rate = 0.1
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, 
+                                     epsilon=1e-9)
+
+temp_learning_rate_schedule = CustomSchedule(d_model)
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+    
+    return tf.reduce_mean(loss_)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+transformer = Transformer(num_layers, d_model, num_heads, dff,
+                          input_vocab_size, target_vocab_size, 
+                          pe_input=input_vocab_size, 
+                          pe_target=target_vocab_size,
+                          rate=dropout_rate)
+
+def create_masks(inp, tar):
+    # Encoder padding mask
+    enc_padding_mask = create_padding_mask(inp)
+    
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    dec_padding_mask = create_padding_mask(inp)
+    
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by 
+    # the decoder.
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+    
+    return enc_padding_mask, combined_mask, dec_padding_mask
+
+checkpoint_path = "./checkpoints/train"
+
+ckpt = tf.train.Checkpoint(transformer=transformer,
+                           optimizer=optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print ('Latest checkpoint restored!!')
+
+EPOCHS = 20
+
+# The @tf.function trace-compiles train_step into a TF graph for faster
+# execution. The function specializes to the precise shape of the argument
+# tensors. To avoid re-tracing due to the variable sequence lengths or variable
+# batch sizes (the last batch is smaller), use input_signature to specify
+# more generic shapes.
+
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+  tar_inp = tar[:, :-1]
+  tar_real = tar[:, 1:]
+  
+  enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+  
+  with tf.GradientTape() as tape:
+    predictions, _ = transformer(inp, tar_inp, 
+                                 True, 
+                                 enc_padding_mask, 
+                                 combined_mask, 
+                                 dec_padding_mask)
+    loss = loss_function(tar_real, predictions)
+
+  gradients = tape.gradient(loss, transformer.trainable_variables)    
+  optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+  
+  train_loss(loss)
+  train_accuracy(tar_real, predictions)
+
+for epoch in range(EPOCHS):
+    start = time.time()
+    
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    
+    # inp -> portuguese, tar -> english
+    for (batch, (query)) in enumerate(query_job.result()):
+        
+        inp, tar = tf_encode(query[2].encode(), query[5].encode())
+        # inp and tar must be
+#         (<tf.Tensor: id=207688, shape=(64, 40), dtype=int64, numpy=
+#  array([[8214, 1259,    5, ...,    0,    0,    0],
+#         [8214,  299,   13, ...,    0,    0,    0],
+#         [8214,   59,    8, ...,    0,    0,    0],
+#         ...,
+#         [8214,   95,    3, ...,    0,    0,    0],
+#         [8214, 5157,    1, ...,    0,    0,    0],
+#         [8214, 4479, 7990, ...,    0,    0,    0]])>,
+#  <tf.Tensor: id=207689, shape=(64, 40), dtype=int64, numpy=
+#  array([[8087,   18,   12, ...,    0,    0,    0],
+#         [8087,  634,   30, ...,    0,    0,    0],
+#         [8087,   16,   13, ...,    0,    0,    0],
+#         ...,
+#         [8087,   12,   20, ...,    0,    0,    0],
+#         [8087,   17, 4981, ...,    0,    0,    0],
+#         [8087,   12, 5453, ...,    0,    0,    0]])>)
+        train_step(inp, tar)
+        
+        if batch % 50 == 0:
+            print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+        
+    if (epoch + 1) % 5 == 0:
+        ckpt_save_path = ckpt_manager.save()
+        print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                            ckpt_save_path))
+        
+    print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1, 
+                                                    train_loss.result(), 
+                                                    train_accuracy.result()))
+
+    print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
